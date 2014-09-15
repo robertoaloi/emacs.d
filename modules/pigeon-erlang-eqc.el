2456d84e8552d9d89b0ddc7138cb23bc31fc217d
Roberto Aloi <robert.aloi@klarna.com>
2014-09-15 10:37:05 +0200

    Whitelist eqc modules for xref

--- modules/pigeon-erlang-eqc.el
+++ modules/pigeon-erlang-eqc.el
@@ -9,5 +9,15 @@
 (setq eqc-max-menu-length 30)
 (setq eqc-root-dir pigeon-erlang-eqc-dir)
 
+;; Do not warn for EQC xref issues
+;; (EQC is compiled without debug_info)
+(defun set-xref-error-whitelist ()
+  (edts-project-set-attribute (eproject-root)
+                              :xref-error-whitelist
+                              '("eqc:" "eqc_gen:" "eqc_statem:")))
+(add-hook 'after-init-hook 'my-after-init-hook)
+(defun my-after-init-hook ()
+  (add-hook 'edts-project-file-visit-hook 'set-xref-error-whitelist))
+
 ;; Provide feature
 (provide 'pigeon-erlang-eqc)
