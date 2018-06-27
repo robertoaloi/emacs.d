0e01ea8955dda9252b2c33d3c6627111dc154a01
Roberto Aloi <robert.aloi@klarna.com>
2018-06-27 08:34:17 +0200

    Install magit

--- modules/pigeon-git.el
+++ modules/pigeon-git.el
@@ -2,6 +2,13 @@
 ;; GIT                                                      ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Install magit
+;; https://magit.vc
+(package-require 'magit)
+
+;; Create a global shortcut for magit-status
+(global-set-key (kbd "C-x g") 'magit-status)
+
 ;; Blame author of a specific line
 (defun blame ()
   "Shows the git-person-to-blame for the current line"
