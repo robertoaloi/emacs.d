e990af3ca74dfbc881d9e24dbd1e93ce77780a7f
Roberto Aloi <robert.aloi@klarna.com>
2021-01-04 14:21:47 +0100

    Elixir LS

--- modules/pigeon-elixir.el
+++ modules/pigeon-elixir.el
@@ -5,5 +5,17 @@
 ;; Activate official package
 (use-package elixir-mode :ensure t)
 
+;; https://github.com/elixir-lsp/elixir-ls
+(use-package lsp-mode
+             :commands lsp
+             :ensure t
+             :diminish lsp-mode
+             :hook
+             (elixir-mode . lsp)
+             :init
+             (add-to-list 'exec-path "~/git/github/elixir-lsp/elixir-ls/rel"))
+
+(add-hook 'elixir-mode-hook #'lsp)
+
 ;; Provide feature
 (provide 'pigeon-elixir)
