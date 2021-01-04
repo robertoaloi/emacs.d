3fd72345880e0a7df47b002391b86938353e2127
Roberto Aloi <robert.aloi@klarna.com>
2021-01-04 15:09:18 +0100

    Do not attempt at installing PDF tools every time

--- modules/pigeon-pdf.el
+++ modules/pigeon-pdf.el
@@ -3,8 +3,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Install PDF Tools
-(package-require 'pdf-tools)
-(pdf-tools-install)
+;; This slows down the init time significantly, so only run it once
+(use-package pdf-tools :ensure t)
+;; (pdf-tools-install)
+
+(add-hook 'doc-view-mode-hook 'pdf-tools-install)
 
 ;; Provide feature
 (provide 'pigeon-pdf)
