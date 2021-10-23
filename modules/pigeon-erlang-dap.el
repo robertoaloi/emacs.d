981b750d85208c85420f6f89f3c86268775e19a3
Roberto Aloi <robert.aloi@klarna.com>
2021-10-23 12:29:05 +0200

    Simplify DAP

--- modules/pigeon-erlang-dap.el
+++ modules/pigeon-erlang-dap.el
@@ -7,29 +7,15 @@
 (setq dap-inhibit-io nil)
 (setq dap-print-io t)
 
+;; Register the Erlang provider
 (defun dap-erlang--populate-start-file-args (conf)
   "Populate CONF with the required arguments."
   (-> conf
-      (dap--put-if-absent :dap-server-path '("els_dap"))
-      (dap--put-if-absent :type "mix_task")
-      (dap--put-if-absent :name "mix test")
-      (dap--put-if-absent :request "launch")
-      (dap--put-if-absent :task "test")
-      (dap--put-if-absent :taskArgs (list "--trace"))
-      (dap--put-if-absent :projectDir (lsp-find-session-folder (lsp-session) (buffer-file-name)))
-      (dap--put-if-absent :cwd (lsp-find-session-folder (lsp-session) (buffer-file-name)))
-      (dap--put-if-absent :requireFiles (list
-                                         "test/**/test_helper.exs"
-                                         "test/**/*_test.exs"))))
-
-(dap-register-debug-provider "Erlang" 'dap-erlang--populate-start-file-args)
-(dap-register-debug-template "Erlang Run Configuration"
-                             (list :type "Erlang"
-                                   :cwd nil
-                                   :request "launch"
-                                   :program "rebar3"
-                                   :args "shell"
-                                   :name "Erlang::Run"))
+    (plist-put :dap-server-path '("els_dap"))
+    (plist-put :projectDir (lsp-find-session-folder (lsp-session) (buffer-file-name)))
+    (plist-put :cwd (lsp-find-session-folder (lsp-session) (buffer-file-name)))
+    ))
+(dap-register-debug-provider "erlang" 'dap-erlang--populate-start-file-args)
 
 ;; Provide feature
 (provide 'pigeon-erlang-dap)
